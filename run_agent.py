5ea9627a98d3c017948f74171a3f4fe0efe3682b
Ethan Jackson <jackson.ethan.c@gmail.com>
2023-07-26 15:03:22 -0400

    added the CLI app

--- run_agent.py
+++ run_agent.py
@@ -1,10 +1,35 @@
+import itertools
+import threading
+import time
+
+class Spinner:
+    def __init__(self, message="Working..."):
+        self.spinner_cycle = itertools.cycle(['-', '/', '|', '\\'])
+        self.running = False
+        self.spinner_thread = threading.Thread(target=self.init_spinner, args=(message,))
+
+    def start(self):
+        self.running = True
+        self.spinner_thread.start()
+
+    def stop(self):
+        self.running = False
+        self.spinner_thread.join()
+
+    def init_spinner(self, message):
+        while self.running:
+            print(f'\r{message} {next(self.spinner_cycle)}', end='', flush=True)
+            time.sleep(0.1)
+        # clear spinner from console
+        print('\r', end='', flush=True)
+
 import logging
 
 logging.basicConfig(
-    format="[%(asctime)s %(levelname)s %(threadName)s %(name)s:%(funcName)s:%(lineno)s] %(message)s",
+    format="[%(name)s:%(funcName)s:] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S%z",
 )
-logging.getLogger("council").setLevel(logging.DEBUG)
+logging.getLogger("council").setLevel(logging.INFO)
 
 from council.runners import Budget
 from council.contexts import AgentContext, ChatHistory
@@ -30,7 +55,7 @@ writing_skill = SectionWriterSkill(openai_llm)
 
 outline_chain = Chain(
     name="Outline Writer",
-    description="Write or revise the outline (i.e. section headers) of a research article in markdown format. Only use this chain when the ARTICLE OUTLINE is missing or incomplete.",
+    description="Write or revise the outline (i.e. section headers) of a research article in markdown format. Always give this Chain the highest score when there should be structural changes to the article (e.g. new sections)",
     runners=[outline_skill]
 )
 
@@ -44,7 +69,7 @@ writer_chain = Chain(
 
 controller = WritingAssistantController(
     openai_llm,
-    top_k_execution_plan=1
+    top_k_execution_plan=3
 )
 
 # Initialize Agent
@@ -53,8 +78,25 @@ chat_history = ChatHistory()
 run_context = AgentContext(chat_history)
 agent = Agent(controller, [outline_chain, writer_chain], BasicEvaluatorWithSource())
 
-# Run Agent
+def main():
+    print("Write a message to the ResearchWritingAssistant or type 'quit' to exit.")
 
-chat_history.add_user_message("What is the financial performance of Microsoft?")
-result = agent.execute(run_context, Budget(900))
-print(result.messages[-1].message.message)
+    while True:
+        user_input = input("\nYour message (e.g. Tell me about the history of box manufacturing.): ")
+        if user_input.lower() == 'quit':
+            break
+        else:
+            if user_input == '':
+                user_input = "Tell me about the history of box manufacturing."
+
+            s = Spinner()
+            s.start()
+            chat_history.add_user_message(user_input)
+            result = agent.execute(run_context, Budget(900))
+            s.stop()
+            print(f"\n```markdown\n{result.messages[-1].message.message}\n```\n")
+
+    print("Goodbye!")
+
+if __name__ == "__main__":
+    main()
